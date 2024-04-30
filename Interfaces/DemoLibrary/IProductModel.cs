7d68e4c56511d5f30fcbf55962bd18edf6ae062e
Nikka Aquino <nikkaaquino31@gmail.com>
2024-04-30 23:54:44 +0800

    Interfaces by Tim Courtney

---
+++ Interfaces/DemoLibrary/IProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoLibrary
+{
+    public interface IProductModel
+    {
+        string Title { get; set; }
+        bool HasOrderBeenCompleted { get; }
+        void ShipItem(CustomerModel customer);
+    }
+}
